75208b6ca13cc06338a36bfeeebeda1030eed90f
Luis Felipe <luis_felipecamara@hotmail.com>
2023-03-06 19:48:20 -0300

    otimizado para producao

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// Ativar Links do menu
+
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// Ativar itens do orçacamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProdutos(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProdutos);
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  console.log(ativa);
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
